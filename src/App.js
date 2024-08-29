431723628def4f1024b6ef3690751b1c3389c09a
Elena Jou <elenajou@Elenas-Air.hitronhub.home>
2024-08-28 23:31:39 -0700

    design revision

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import Landing from './components/Landing/landing.js';
+import Countdown from './components/Countdown/countdown.js';
 import Invitation from './components/Invitation/invitation.js';
+import Retratos from './components/Retratos/retratos.js';
+import MoreInfo from './components/MoreInfo/moreinfo.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div class="App">
       {/* <header className="App-header"></header> */}
+      <Landing/>
+      <Countdown/>
+      <Retratos/>
       <Invitation/>
+      <MoreInfo/>
     </div>
   );
 }
