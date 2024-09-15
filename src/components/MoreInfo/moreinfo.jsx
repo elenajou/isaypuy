2fb7d3bfe776b3aa87a8ac2743663ceab68c5489
Elena Jou <elenajou.ej@gmail.com>
2024-09-14 17:10:12 -0700

    Added modals for retratos

--- src/components/MoreInfo/moreinfo.jsx
+++ src/components/MoreInfo/moreinfo.jsx
@@ -5,8 +5,10 @@ import CustomModal from '../CustomModal/customModal.jsx';
 
 import './moreinfo.css';
 
-let dressCodeHTML = <><div>I am a dress code modal</div></>;
-let infoModalHTML = <><div>I am a tips modal</div></>;
+let dressCodeHTML = <div>I am a dress code modal</div>;
+let infoModalHTML = <div>I am a tips modal</div>;
+let dressCodeBtn = <div>+ INFO</div>;
+let infoBtn = <div>SUGERENCIAS</div>;
 
 function MoreInfo() {
   return (
@@ -15,13 +17,13 @@ function MoreInfo() {
         <div className="card-title">Dress Code</div>
         <div className="card-icon"><img src={tie}/></div>
         <div className="card-caption">Una orientacion para tu vestuario</div>
-        <CustomModal buttonID="sugerenciaModal" content={dressCodeHTML} caption="SUGERENCIAS" classes="card-button"></CustomModal>
+        <CustomModal buttonID="sugerenciaModal" content={dressCodeHTML} caption={infoBtn} classes="button card-button"></CustomModal>
       </div>
       <div class="card">
         <div className="card-title">Tips y Notas</div>
         <div className="card-icon"><img src={notes}/></div>
         <div className="card-caption">Information adicional para tener en cuenta</div>
-        <CustomModal buttonID="infoModal" content={infoModalHTML} caption="+ INFO" classes="card-button"></CustomModal>
+        <CustomModal buttonID="infoModal" content={infoModalHTML} caption={dressCodeBtn} classes="button  card-button"></CustomModal>
       </div>
     </div>
   );
