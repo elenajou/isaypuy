26d25ebbcfdaee54bd72ed2645467ab986049662
Elena Jou <elenajou.ej@gmail.com>
2024-09-21 17:10:59 -0700

    added few examples

--- src/components/Invitation/invitation.jsx
+++ src/components/Invitation/invitation.jsx
@@ -27,19 +27,29 @@ function Invitation() {
     }, 500); // Adjust delay as needed
   };
 
-  const searchHTML = <div class="modal-body">
-    <div class="rsvp-title">RSVP</div>
-    <input type="text" name="search-name" id="rsvp-search-name" class="rsvp-input" placeholder='Nombre'/>
-    <input type="text" name="search-number" id="rsvp-search-number" class="rsvp-input" placeholder='Numero WhatsApp'/>
-    <div class="rsvp-btn">
-      <button class="button" onClick={toggleInvitacionModalContent}>BUSCAR</button>
-    </div>
-  </div>;
+  // const searchHTML = <div class="modal-body">
+  //   <div class="rsvp-title">RSVP</div>
+  //   <input type="text" name="search-name" id="rsvp-search-name" class="rsvp-input" placeholder='Nombre'/>
+  //   <input type="text" name="search-number" id="rsvp-search-number" class="rsvp-input" placeholder='Numero WhatsApp'/>
+  //   <div class="rsvp-btn">
+  //     <button class="button" onClick={toggleInvitacionModalContent}>BUSCAR</button>
+  //   </div>
+  // </div>;
 
-  const invitacionHTML = <div class="modal-body">
+  const searchHTML = <form class="rsvp-modal-body form-inputs" method="post" action="javascript:populateData();">
+    <div class="rsvp-title">RSVP</div>
+    <input type="text" id="rsvp-search-name" class="rsvp-input" name="name" placeholder="Nombre / Name" required />
+    <input type="number" id="rsvp-search-number" class="rsvp-input" name="whatsapp" 
+    placeholder="Whatsapp / Phone Number" required min="60000000" max="9999999999"/>
+    <button type="submit" onClick={toggleInvitacionModalContent} id="populateBtn" class="button rsvp-btn">BUSCAR</button>
+    <div id="populateField"></div>
+    <div id="searchMsg"></div>
+  </form>;
+  
+  const invitacionHTML = <div class="rsvp-modal-body">
     <div class="rsvp-title">RSVP</div>
     <div class="rsvp-name">Elena Jou</div>
-    <div class="rsvp-contact"><div>Numero de Contacto:</div><div> 0000-0000</div></div>
+    <div class="rsvp-phone"><div>Numero de Contacto:</div><div> 0000-0000</div></div>
     <div class="rsvp-seats"><div>Puestos Reservados:</div><div> 2</div></div>
     <label for="rsvp-msg" class="rsvp-caption">Deja un mensaje</label>
     <input type="text" id="rsvp-msg"/>
@@ -47,6 +57,10 @@ function Invitation() {
       <button class="button" onClick={toggleSearchModalContent}>Estare ahi!</button>
       <button class="button">No puedo :&#40;</button>
     </div>
+  </div>;
+
+  const lugarHTML = <div class="rsvp-modal-body">
+    <div class="rsvp-title">TBD</div>
   </div>;
 
   // Default content for modal is 'ceremoniaHTML'
